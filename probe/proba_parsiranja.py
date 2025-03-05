89d56ff50dcaa82c1092c58bd4c8f581c6626cbf
DervisevicDino <dino.dervisevic@fer.hr>
2025-03-05 12:53:18 +0100

    Initial commit

---
+++ probe/proba_parsiranja.py
@@ -0,0 +1,21 @@
+import zipfile
+import os
+
+def extract_llsp(file_path, output_dir):
+    with zipfile.ZipFile(file_path, 'r') as zip_ref:
+        zip_ref.extractall(output_dir)
+
+def extract_sb3(file_path, output_dir):
+    with zipfile.ZipFile(file_path, 'r') as zip_ref:
+        zip_ref.extractall(output_dir)
+
+# Putanja do .llsp datoteke
+llsp_file_path = "C:/Users/amrad/OneDrive/Documents/LEGO Education SPIKE/Project_2.llsp3"
+
+output_dir = 'C:/Users/amrad/OneDrive/Documents/LEGO Education SPIKE'
+
+sb3_path = "C:/Users/amrad/OneDrive/Documents/LEGO Education SPIKE/scratch.sb3"
+
+# Ekstraktiraj .llsp datoteku
+extract_sb3(llsp_file_path, output_dir)
+extract_sb3(sb3_path, output_dir)
